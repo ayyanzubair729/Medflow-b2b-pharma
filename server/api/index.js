be20fb725f51804d2193588cf2c96d8ab097d5a2
Ayyan Zubair <ayyanzubair729@gmail.com>
2026-05-20 18:07:26 +0500

    final changess

--- server/api/index.js
+++ server/api/index.js
@@ -18,7 +18,7 @@ const dbReady = AppDataSource.isInitialized
 
 export default async function (req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
 
   if (req.method === "OPTIONS") {
