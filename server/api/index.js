4fee659ae4a13bd7c4bb8f8a717417d15d478e92
Ayyan Zubair <ayyanzubair729@gmail.com>
2026-05-20 16:23:57 +0500

    fix: export Express app directly without serverless-http

--- server/api/index.js
+++ server/api/index.js
@@ -2,31 +2,8 @@ import "reflect-metadata";
 import { AppDataSource } from "../src/config/data-source.js";
 import app from "../src/app.js";
 
-let handler;
+AppDataSource.initialize()
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.error("DB error:", err));
 
-function setCors(res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-}
-
-export default async function (req, res) {
-  setCors(res);
-
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-
-  try {
-    if (!handler) {
-      await AppDataSource.initialize();
-      const { default: serverless } = await import("serverless-http");
-      handler = serverless(app);
-    }
-    return handler(req, res);
-  } catch (err) {
-    res.status(500).json({ error: "Server init failed" });
-  }
-}
+export default app;
